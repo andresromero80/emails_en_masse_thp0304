7686187f5b5968e6cfa25a04b776544818533779
Alexandre Peccard <alexandre.peccard@gmail.com>
2018-10-18 12:28:35 +0200

    Bases of the application building

---
+++ lib/views/done.rb
@@ -0,0 +1,16 @@
+class Done
+	attr_accessor
+
+	def initialize
+
+	end
+
+	def end
+		puts "Work is done."
+		puts "Thanks you for using this software"
+		puts "Please send us any suggestion at thp-google-agent.fr"
+		puts "Program will now exit. See you soon !"
+
+		system.exit
+	end
+end
